7d342663b7aec444ea915d1448642d02906dc03c
汪翰林 <wanghanlin@wanghanlindeMacBook-Pro.local>
2024-05-20 22:06:43 -0700

    update event

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -43,6 +43,42 @@ const Event = () => (
           </Card>
         </Col>
       </Row>
+      <Row>
+        <Col md={6} className="mb-4">
+          <Card className="h-100 hover-card">
+            <Card.Body>
+              <Card.Title>活动通知</Card.Title>
+              <Card.Text>
+                <strong>主题：</strong>萨嘎达瓦日<br />
+                <strong>地点：</strong>福增佛堂 TH-9 3200 Corvette Way, Richmond<br />
+                <strong>时间：</strong>2024年5月23日（周四）11:00 AM - 3:00 PM<br />
+                <strong>内容：</strong>供养佛法僧（供花，供果，供素食，持咒等）<br />
+                <strong>萨嘎达瓦节：</strong>又称佛吉祥日，与佛陀一生中三件重要的大事（诞生、成道、涅槃）联系在一起的神圣日子。<br />
+                <strong>观音心咒：</strong>唵嘛呢叭咪吽<br />
+                <strong>欢迎词：</strong>欢迎有空的师兄，周四来佛堂积极参与。<br />
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col md={6} className="mb-4">
+          <Card className="h-100 hover-card">
+            <Card.Body>
+              <Card.Title>法讯</Card.Title>
+              <Card.Text>
+                <strong>内容：</strong>《百业经》共修课程<br />
+                <strong>温哥华时间：</strong>2024年5月23号(周四)晚上 8 点<br />
+                <strong>北京时间：</strong>2024年5月24号(周五)早上11点<br />
+                <strong>会议ID：</strong>846 7801 2384<br />
+                <strong>密码：</strong>0523<br />
+                <strong>信息：</strong>一切众生最大的灾难是没有机缘得闻佛法。<br />
+                <strong>感恩：</strong>感恩上师，弘扬佛法，利益众生。<br />
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
     </Container>
     <CustomFooter />
   </div>
